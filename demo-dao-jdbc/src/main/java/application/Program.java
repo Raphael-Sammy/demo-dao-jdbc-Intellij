ca7a082eee053795cc71f336617033024be20284
Raphael-Sammy <133025924+Raphael-Sammy@users.noreply.github.com>
2023-11-29 16:03:37 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/main/java/application/Program.java
+++ demo-dao-jdbc/src/main/java/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,7 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(), 3000.0, obj);
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 }
---
+++ demo-dao-jdbc/src/main/java/model/dao/SellerDaoJDBC.java
@@ -0,0 +1,28 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao{
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Seller id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
