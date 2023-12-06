ca1437cf52585582739893673c499f3c019c0c87
Raphael-Sammy <133025924+Raphael-Sammy@users.noreply.github.com>
2023-12-06 10:52:49 -0300

    Reusing Instantiation

--- demo-dao-jdbc/src/main/java/application/Program.java
+++ demo-dao-jdbc/src/main/java/application/Program.java
@@ -2,17 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.SellerDaoJDBC;
-import model.entities.Department;
-import model.entities.Seller;
 
-import java.util.Date;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(), 3000.0, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 }
--- demo-dao-jdbc/src/main/java/model/dao/SellerDao.java
+++ demo-dao-jdbc/src/main/java/model/dao/SellerDao.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface SellerDao {
     void update(Seller obj);
     void deleteById(Integer id);
-    Seller findById(Seller id);
+    Seller findById(Integer id);
     List<Seller> findAll();
 }
